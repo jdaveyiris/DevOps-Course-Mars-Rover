dc9c5b774dd0e141c94ce0199817b14acb7ed128
Jim Davey <jim.davey@iris.co.uk>
2021-12-07 15:57:58 +0000

    Initial set up working
    Co-authored-by: Chris Sweeney <ChrisSweeneyIRIS@users.noreply.github.com>
    Co-authored-by: jknights2000 <jknights2000@users.noreply.github.com>
    Co-authored-by: FrancisIris <FrancisIris@users.noreply.github.com>
    Co-authored-by: Hugh Emerson <hugh-emerson@users.noreply.github.com>

--- app/app.py
+++ app/app.py
@@ -1,12 +1,16 @@
 from flask import Flask, render_template, request, redirect, url_for
-
+import requests
+import datetime
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def index():
-    return render_template('index.html', landing_image='https://via.placeholder.com/500')
+    current_date=datetime.datetime.now().strftime("%Y-%m-%d")
+    rover = request.args.get("rover","curiosity")
+    date = request.args.get("Date","2015-12-03")
+    response = requests.get(f"https://api.nasa.gov/mars-photos/api/v1/rovers/{rover}/photos?earth_date={date}&api_key=DEMO_KEY")
+    data = response.json()
+    
+    return render_template('index.html', landing_image=data["photos"][0]["img_src"], current_date=current_date)
 
-@app.route('/mars')
-def mars():
-    return render_template('mars.html')
